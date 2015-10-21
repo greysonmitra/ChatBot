e3c503ca6242afaca8fd14c9f4a2bb3c8479a655
greysonmitra <greyson.mitra@outlook.com>
2015-10-21 08:31:34 -0600

    Created basic classes for project.
    Also copied/typed basic code for checkers and controller.

---
+++ src/chat/controller/ChatbotController.java
@@ -0,0 +1,14 @@
+package chat.controller;
+
+/**
+ *Application controller for the Chatbot project. 
+ * @author gmit3891
+ *@version 1.0 10/21/15
+ */
+public class ChatbotController
+{
+	public void start()
+	{
+		
+	}
+}
---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,14 @@
+package chat.controller;
+/**
+ * Starts the Chatbot program
+ * @author gmit3891
+ *@version 1.0 10/21/15
+ */
+public class ChatbotRunner
+{
+	public static void main (String [] args)
+	{
+		ChatbotController myController = new ChatbotController();
+		myController.start();
+	}
+}
