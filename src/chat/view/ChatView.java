a50d474399053bb6dc322fb4f11dcdf87b8d0023
greysonmitra <greyson.mitra@outlook.com>
2015-11-05 08:51:25 -0700

    Successfully added Icon for popups.
    Downloaded an Icon to use for the project popups.

--- src/chat/view/ChatView.java
+++ src/chat/view/ChatView.java
@@ -1,9 +1,25 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
-
+import javax.swing.ImageIcon;
+/**
+ * A display that uses popups to take user input for the Chatbot project.
+ * @author gmit3891
+ *1.3 11/5/15 Added Icons to popup windows!
+ */
 public class ChatView
 {
+
+	private String popupMessage;
+	private ImageIcon chatbotIcon;
+	
+	public ChatView()
+	{
+		popupMessage = "Hello, this is your friend Gorillabot";
+		chatbotIcon = new ImageIcon(getClass().getResource("Images/FRIKINWORK.jpg"));
+	}
+	
+	
 	public void showResponse(String displayText)
 	{
 		JOptionPane.showMessageDialog(null, displayText);
@@ -13,11 +29,15 @@ public class ChatView
 	{
 		String returnedText = "";
 		
-		returnedText = JOptionPane.showInputDialog(null, displayText) ;
+		returnedText = JOptionPane.showInputDialog(null, displayText, popupMessage, JOptionPane.PLAIN_MESSAGE, chatbotIcon, null, "Answer here").toString(); ;
 		
 		return returnedText;
 	}
 	
+	public void displayText(String displayText)
+	{
+		JOptionPane.showMessageDialog(null, displayText, popupMessage, JOptionPane.INFORMATION_MESSAGE, chatbotIcon);
+	}
 //	public void showResponse(String displayText)
 //	{
 //		JOptionPane.showMessageDialog(null, displayText);
