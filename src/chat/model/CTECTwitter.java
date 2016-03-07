7eea8ca227c2e7d3ec5f35fa404d88cb7d0ca90e
greysonmitra <greyson.mitra@outlook.com>
2016-03-07 09:00:44 -0700

    Declarations for ArrayLists needed for saving tweets.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,15 +1,19 @@
 package chat.model;
 
 import java.util.ArrayList;
+import twitter4j.*;
 
 /**
  * 
  * @author gmit3891
- * @version 0.1
+ * @version 0.2 ArrayLists for T4J
  * Writing code for interaction between our Chatbot and Twitter. 
  */
 public class CTECTwitter
 {
+	//Declarations
 	
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
 	
 }
