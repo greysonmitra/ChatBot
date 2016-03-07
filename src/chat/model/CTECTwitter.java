2d464190de576e11c9042e7710488b9faaf1c95d
greysonmitra <greyson.mitra@outlook.com>
2016-03-07 09:05:42 -0700

    The code now constructs our ArrayLists

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -6,7 +6,7 @@ import twitter4j.*;
 /**
  * 
  * @author gmit3891
- * @version 0.2 ArrayLists for T4J
+ * @version 0.3 Constructs our ArrayLists now.
  * Writing code for interaction between our Chatbot and Twitter. 
  */
 public class CTECTwitter
@@ -15,5 +15,13 @@ public class CTECTwitter
 	
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+		
+	}
 	
 }
